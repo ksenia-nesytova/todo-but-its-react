cf3ced1929405ce498a8388839a13332c891df39
ksenia-nesytova <alleheldentot@gmail.com>
2021-08-05 17:05:08 +0700

    added and styled Edit btn

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 
 function TodoItem(props) {
   const [checked, setChecked] = useState(false);
+  const [isBeingEdited, setBeingEdited] = useState(false);
+  const [newtext, setNewText] = useState('');
 
   return(
     <div className="todoItem">
       <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
       <p>{props.task.text}</p>
-      <button type="button" className="deleteTodo" onClick={() => props.deleteTask(props.id)}>x</button>
+      <button type="button" className="deleteTodo" onClick={() => props.deleteTask(props.id)}>x</button> <button type="button" className="editTodo" onClick={() => props.editTask(props.id)}>edit</button>
     </div>
   )
 }
