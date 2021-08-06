02bf72e0d038a0d514e81af6f48fbacaee9fd60b
ksenia-nesytova <alleheldentot@gmail.com>
2021-08-06 13:34:26 +0700

    can now edit and save edited todos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,15 +3,37 @@ import React, { useState } from "react";
 function TodoItem(props) {
   const [checked, setChecked] = useState(false);
   const [isBeingEdited, setBeingEdited] = useState(false);
-  const [newtext, setNewText] = useState('');
+  const [newText, setNewText] = useState('');
 
-  return(
+function handleChange(e) {
+  setNewText(e.target.value);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  props.editTask(props.id, newText);
+  setNewText(newText)
+  setBeingEdited(false)
+
+}
+
+  const displayTemplate = (
     <div className="todoItem">
       <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
       <p>{props.task.text}</p>
-      <button type="button" className="deleteTodo" onClick={() => props.deleteTask(props.id)}>x</button> <button type="button" className="editTodo" onClick={() => props.editTask(props.id)}>edit</button>
+      <button type="button" className="deleteTodo" onClick={() => props.deleteTask(props.id)}>x</button>
+      <button type="button" className="editTodo" onClick={() => setBeingEdited(true)}>edit</button>
     </div>
   )
+  const editingTemplate = (
+    <div className="todoItem">
+      <input type="text" onChange={handleChange}/>
+      <p>{props.task.text}</p>
+      <button type="submit" className="editSave" onClick={handleSubmit}>save</button>
+      <button type="button" className="editCancel" onClick={() => setBeingEdited(false)}>cancel</button>
+    </div>
+  )
+  return(isBeingEdited ? editingTemplate : displayTemplate)
 }
 
 
