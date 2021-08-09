22e3cc22137d32b5a3cb0882a268bede9825442f
ksenia-nesytova <alleheldentot@gmail.com>
2021-08-09 16:08:39 +0700

    clicking on Edit btn now moves focus to input

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,39 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function TodoItem(props) {
   const [completed, setCompleted] = useState(false);
   const [isBeingEdited, setBeingEdited] = useState(false);
   const [newText, setNewText] = useState(props.task.text);
 
+  const inputRef = useRef(null);
+  const buttonRef = useRef(null);
 
-function handleChange(e) {
-  setNewText(e.target.value);
-}
+  function handleChange(e) {
+    setNewText(e.target.value);
+  }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  props.editTask(props.id, newText);
-  setNewText(newText)
-  setBeingEdited(false)
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newText);
+    setNewText(newText)
+    setBeingEdited(false)
 
-}
+  }
 
-  const displayTemplate = (
-    <div className="todoItem">
-      <input type="checkbox" checked={props.completed} onChange={() =>  props.toggleTaskCompleted(props.id)}/>
-      <p>{props.task.text}</p>
-      <button type="button" onClick={() => props.deleteTask(props.id)}>x</button>
-      <button type="button" onClick={() => setBeingEdited(true)}>edit</button>
-    </div>
-  )
-  const editingTemplate = (
-    <div className="todoItem">
-      <input type="text" defaultValue={props.task.text} value={newText} onChange={handleChange}/>
-      <button type="submit" onClick={handleSubmit}>save</button>
-      <button type="button" onClick={() => setBeingEdited(false)}>cancel</button>
-    </div>
-  )
-  return(isBeingEdited ? editingTemplate : displayTemplate)
+    const displayTemplate = (
+      <div className="todoItem">
+        <input type="checkbox" checked={props.completed} onChange={() =>  props.toggleTaskCompleted(props.id)}/>
+        <p>{props.task.text}</p>
+        <button type="button" onClick={() => props.deleteTask(props.id)}>x</button>
+        <button type="button" onClick={() => setBeingEdited(true)} ref={buttonRef}>edit</button>
+      </div>
+    )
+    const editingTemplate = (
+      <div className="todoItem">
+        <input
+          id={props.id}
+         type="text"
+         defaultValue={props.task.text}
+         value={newText}
+         onChange={handleChange}
+         ref={inputRef}/>
+        <button type="submit" onClick={handleSubmit}>save</button>
+        <button type="button" onClick={() => setBeingEdited(false)}>cancel</button>
+      </div>
+    )
+
+    useEffect(() => {
+      if(isBeingEdited) {
+        inputRef.current.focus();
+      }
+    }, [isBeingEdited])
+
+    return(isBeingEdited ? editingTemplate : displayTemplate)
 }
 
 
